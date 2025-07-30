be405045a7bd71436daeffce590acd490cbf37ec
Zatigan <joe.azerty@tutanota.de>
2025-07-30 16:00:16 +0200

    Feat: index.js created + packages npm

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 
-//* Used to navigate on website
-const router = require("./app/router");
-
-//* Path for dirname use
-const path = require("path");
-
 //* Express to handle server
 const express = require("express");
 const app = express();
 
-//* Define view engine + path for views
-app.set("view engine", "ejs");
-app.set("views", "app/views");
-
-//* Path for public elem
-app.use(express.static("public"));
+//* Used to navigate on website
+const router = require("./app/router");
 
 //*Routing
 app.use(router);
+
+//* Path for dirname use
+const path = require("path");
+
+//* Define view engine + path for views
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "app/views"));
+
+//* Path for public elem
+app.use(express.static("public"));
 
 //* Page 404 tbd
 

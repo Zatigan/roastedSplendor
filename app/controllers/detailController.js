e278e93914c3089bfda248389cf232fcb82c9860
Zatigan <joe.azerty@tutanota.de>
2025-08-11 22:22:50 +0200

    Feat: updating titles + new controller

---
+++ app/controllers/detailController.js
@@ -0,0 +1,1 @@
+import dataMapper from "../datamapperr/datamapper.js";
--- app/datamapper/datamapper.js
+++ app/datamapper/datamapper.js
@@ -1,19 +1,22 @@
-import client from "../database.js"
+import client from "../database.js";
 
 const dataMapper = {
-
-async getThreeCoffees() {
-    const result = await client.query("SELECT * FROM coffee ORDER BY date DESC LIMIT 3");
+  async getThreeCoffees() {
+    const result = await client.query(
+      "SELECT * FROM coffee ORDER BY date DESC LIMIT 3"
+    );
     return result.rows;
-},
+  },
 
-async getAllCoffees() {
+  async getAllCoffees() {
     const result = await client.query("SELECT * FROM coffee");
     return result.rows;
-}
+  },
 
-
-
+  async getOneCofffee() {
+    const result = await client.query("SELECT * FROM coffees WHERE name = $1");
+    return result.rows;
+  },
 };
 
 export default dataMapper;
